82b86001457415468ea1735371af71b7295e289c
Bryan Salazar <bryanmtdt@gmail.com>
2017-08-24 12:54:15 -0600

    PCB Boton Simple Bv0.3

--- Boton/.ino.cpp
+++ Boton/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-07-13 12:29:58
+//This file has been generated on 2017-08-05 17:38:45
 
 #include "Arduino.h"
 #include "Arduino.h"
